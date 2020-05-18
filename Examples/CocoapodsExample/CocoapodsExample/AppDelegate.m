80bbf543e64c6961fdafb8e850c9d4349e1fa5cc
Marius Andra <marius.andra@gmail.com>
2020-05-18 13:39:31 +0200

    update pod example

--- Examples/CocoapodsExample/CocoapodsExample/AppDelegate.m
+++ Examples/CocoapodsExample/CocoapodsExample/AppDelegate.m
@@ -6,7 +6,7 @@
 
 @end
 
-NSString *const POSTHOG_API_KEY = @"zr5x22gUVBDM3hO3uHkbMkVe6Pd6sCna";
+NSString *const POSTHOG_API_KEY = @"BrpS4SctoaCCsyjlnlun3OzyNJAafdlv__jUWaaJWXg";
 
 
 @implementation AppDelegate
@@ -17,6 +17,7 @@ NSString *const POSTHOG_API_KEY = @"zr5x22gUVBDM3hO3uHkbMkVe6Pd6sCna";
     [PHGPostHog debug:YES];
     PHGPostHogConfiguration *configuration = [PHGPostHogConfiguration configurationWithApiKey:POSTHOG_API_KEY host:@"https://app.posthog.com"];
     configuration.captureApplicationLifecycleEvents = YES;
+    configuration.recordScreenViews = YES;
     configuration.flushAt = 1;
     [PHGPostHog setupWithConfiguration:configuration];
     [[PHGPostHog sharedPostHog] identify:@"Prateek" properties:nil options: @{@"$anon_distinct_id":@"test_anonymousId"}];

0db66c3c70b18a6a7f51d5e61874e098fe8b303e
Manoel Aranda Neto <marandaneto@gmail.com>
2023-10-24 13:55:54 +0200

    remove nsdata from tests

--- PostHogTests/PostHogTest.swift
+++ PostHogTests/PostHogTest.swift
@@ -51,8 +51,6 @@ class PostHogTest: QuickSpec {
 
             expect(anonymousId) == otherAnonymousId
             expect(distinctId) == otherDistinctId
-            expect(sessionId) != otherSessionId
-            expect(refreshedSessionId) == otherSessionId
         }
 
 //    it("fires Application Opened for UIApplicationDidFinishLaunching") {

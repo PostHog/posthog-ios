7edc90ca1e282485163f226556c0660b5f94ae3a
Manoel Aranda Neto <marandaneto@gmail.com>
2023-11-03 14:23:46 +0100

    Fix update tag script to checkout to the target branch

--- scripts/update-tag.sh
+++ scripts/update-tag.sh
@@ -3,12 +3,13 @@ set -euo pipefail
 
 CURRENT_TAG="$1"
 TEMP_TAG="${CURRENT_TAG}-temp"
+GITHUB_BRANCH="${2}"
 
 echo "Going to push the tag changes."
 git config --global user.name 'PostHog Github Bot'
 git config --global user.email 'github-bot@posthog.com'
 git fetch
-git checkout ${CURRENT_TAG}
+git checkout ${GITHUB_BRANCH}
 # Create a new temporary tag after pushing the changes from the previous tag (bump version)
 git tag -a ${TEMP_TAG} -m "${TEMP_TAG}"
 # Remove the current tag

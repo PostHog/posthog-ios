b2a060de48441db7945229f458cb1ec6feed0b16
Manoel Aranda Neto <marandaneto@gmail.com>
2024-02-09 12:01:16 +0100

    fix

--- PostHog/Utils/DateUtils.swift
+++ PostHog/Utils/DateUtils.swift
@@ -7,21 +7,22 @@
 
 import Foundation
 
-// returns 2024-02-09T10:53:53.781Z
-public func toISO8601String(_ date: Date) -> String {
+private func newDateFormatter() -> DateFormatter {
     let dateFormatter = DateFormatter()
     dateFormatter.locale = Locale(identifier: "en_US_POSIX")
     dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
 
     dateFormatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss.SSS'Z'" // Use 'hh' for 12-hour clock
+    return dateFormatter
+}
+
+// returns 2024-02-09T10:53:53.781Z
+public func toISO8601String(_ date: Date) -> String {
+    let dateFormatter = newDateFormatter()
     return dateFormatter.string(from: date)
 }
 
 public func toISO8601Date(_ date: String) -> Date? {
-    let dateFormatter = DateFormatter()
-    dateFormatter.locale = Locale(identifier: "en_US_POSIX")
-    dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
-
-    dateFormatter.dateFormat = "yyyy-MM-dd'T'hh:mm:ss.SSS'Z'" // Use 'hh' for 12-hour clock
+    let dateFormatter = newDateFormatter()
     return dateFormatter.date(from: date)
 }

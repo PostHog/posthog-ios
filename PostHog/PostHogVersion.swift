49217934452aec8f9ffc4a529b6089a62cf19569
Manoel Aranda Neto <marandaneto@gmail.com>
2024-07-26 12:59:46 -0300

    Update version

--- PostHog/PostHogVersion.swift
+++ PostHog/PostHogVersion.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // if you change this, make sure to also change it in the podspec and check if the script scripts/bump-version.sh still works
 // This property is internal only
-public var postHogVersion = "3.6.2"
+public var postHogVersion = "3.6.3"
 
 // This property is internal only
 public var postHogSdkName = "posthog-ios"

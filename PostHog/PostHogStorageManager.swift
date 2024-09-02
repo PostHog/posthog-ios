230ba7d3f21c37b3ea53bb225a3073581577059a
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2024-09-02 18:10:23 +0200

    chore: cache flags, distinct id and anon id in memory to avoid file IO every time (#177)

--- PostHog/PostHogStorageManager.swift
+++ PostHog/PostHogStorageManager.swift
@@ -14,20 +14,28 @@ class PostHogStorageManager {
     private let distinctLock = NSLock()
     private let idGen: (UUID) -> UUID
 
+    private var distinctId: String?
+    private var cachedDistinctId = false
+    private var anonymousId: String?
+
     init(_ config: PostHogConfig) {
         storage = PostHogStorage(config)
         idGen = config.getAnonymousId
     }
 
     public func getAnonymousId() -> String {
-        var anonymousId: String?
         anonLock.withLock {
-            anonymousId = storage.getString(forKey: .anonymousId)
-
             if anonymousId == nil {
-                let uuid = UUID.v7()
-                anonymousId = idGen(uuid).uuidString
-                setAnonId(anonymousId ?? "")
+                var anonymousId = storage.getString(forKey: .anonymousId)
+
+                if anonymousId == nil {
+                    let uuid = UUID.v7()
+                    anonymousId = idGen(uuid).uuidString
+                    setAnonId(anonymousId ?? "")
+                } else {
+                    // update the memory value
+                    self.anonymousId = anonymousId
+                }
             }
         }
 
@@ -41,19 +49,40 @@ class PostHogStorageManager {
     }
 
     private func setAnonId(_ id: String) {
+        anonymousId = id
         storage.setString(forKey: .anonymousId, contents: id)
     }
 
     public func getDistinctId() -> String {
         var distinctId: String?
         distinctLock.withLock {
-            distinctId = storage.getString(forKey: .distinctId) ?? getAnonymousId()
+            if self.distinctId == nil {
+                // since distinctId is nil until its identified, no need to read from
+                // cache every single time, otherwise anon users will never used the
+                // cached values
+                if !cachedDistinctId {
+                    distinctId = storage.getString(forKey: .distinctId)
+                    cachedDistinctId = true
+                }
+
+                // do this to not assign the AnonymousId to the DistinctId, its just a fallback
+                if distinctId == nil {
+                    distinctId = getAnonymousId()
+                } else {
+                    // update the memory value
+                    self.distinctId = distinctId
+                }
+            } else {
+                // read from memory
+                distinctId = self.distinctId
+            }
         }
         return distinctId ?? ""
     }
 
     public func setDistinctId(_ id: String) {
         distinctLock.withLock {
+            distinctId = id
             storage.setString(forKey: .distinctId, contents: id)
         }
     }
@@ -61,9 +90,12 @@ class PostHogStorageManager {
     public func reset() {
         distinctLock.withLock {
             storage.remove(key: .distinctId)
+            distinctId = nil
+            cachedDistinctId = false
         }
         anonLock.withLock {
             storage.remove(key: .anonymousId)
+            anonymousId = nil
         }
     }
 }

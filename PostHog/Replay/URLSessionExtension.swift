91311f5a3853bc1116b8ac80bcc2e4f705323f8c
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-11 09:27:01 +0200

    chore: add urlsession extension for async methods

---
+++ PostHog/Replay/URLSessionExtension.swift
@@ -0,0 +1,79 @@
+//
+//  URLSessionExtension.swift
+//  PostHog
+//
+//  Created by Manoel Aranda Neto on 10.09.24.
+//
+
+import Foundation
+
+public extension URLSession {
+    func postHogData(for request: URLRequest) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var currentResponse: URLResponse?
+        var after: Date?
+        do {
+            let (data, response) = try await self.data(for: request)
+            after = Date()
+            currentResponse = response
+
+            captureData(request: request, response: response, before: before, after: after)
+
+            return (data, response)
+        } catch {
+            captureData(request: request, response: currentResponse, before: before, after: after)
+            throw error
+        }
+    }
+
+    func postHogData(from url: URL) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var currentResponse: URLResponse?
+        var after: Date?
+        do {
+            let (data, response) = try await self.data(from: url)
+            let after = Date()
+            currentResponse = response
+
+            captureData(request: nil, response: response, before: before, after: after)
+
+            return (data, response)
+        } catch {
+            captureData(request: nil, response: currentResponse, before: before, after: after)
+            throw error
+        }
+    }
+
+    private func captureData(request: URLRequest? = nil, response: URLResponse? = nil, before: Date, after: Date? = nil) {
+        // we dont check config.sessionReplayConfig.captureNetworkTelemetry here since this extension
+        // has to be called manually anyway
+        if !PostHogSDK.shared.isSessionReplayActive() {
+            return
+        }
+        let currentAfter = after ?? Date()
+
+        PostHogReplayIntegration.dispatchQueue.async {
+            var snapshotsData: [Any] = []
+
+            var requestsData: [String: Any] = ["duration": currentAfter.toMillis() - before.toMillis(),
+                                               "method": request?.httpMethod ?? "GET",
+                                               "name": request?.url?.absoluteString ?? (response?.url?.absoluteString ?? ""),
+                                               "initiatorType": "fetch",
+                                               "entryType": "resource",
+                                               "transferSize": response?.expectedContentLength ?? 0,
+                                               "timestamp": before.toMillis()]
+
+            if let urlResponse = response as? HTTPURLResponse {
+                requestsData["responseStatus"] = urlResponse.statusCode
+            }
+
+            let payloadData: [String: Any] = ["requests": requestsData]
+            let pluginData: [String: Any] = ["plugin": "rrweb/network@1", "payload": payloadData]
+
+            let recordingData: [String: Any] = ["type": 6, "data": pluginData, "timestamp": currentAfter.toMillis()]
+            snapshotsData.append(recordingData)
+
+            PostHogSDK.shared.capture("$snapshot", properties: ["$snapshot_source": "mobile", "$snapshot_data": snapshotsData])
+        }
+    }
+}

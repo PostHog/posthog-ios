b69bbaad42d95f08dae35aefe95fb1fbdab26bb3
Manoel Aranda Neto <marandaneto@gmail.com>
2024-09-12 11:34:04 +0200

    fix

--- PostHog/Replay/URLSessionExtension.swift
+++ PostHog/Replay/URLSessionExtension.swift
@@ -1,12 +1,3 @@
-//
-//  URLSessionExtension.swift
-//  PostHog
-//
-//  Created by Manoel Aranda Neto on 10.09.24.
-//
-
-import Foundation
-
 public extension URLSession {
     func postHogData(for request: URLRequest) async throws -> (Data, URLResponse) {
         let before = Date()
@@ -14,9 +5,7 @@ public extension URLSession {
         do {
             let (data, response) = try await self.data(for: request)
             after = Date()
-
             captureData(request: request, response: response, before: before, after: after)
-
             return (data, response)
         } catch {
             captureData(request: request, response: nil, before: before, after: after)
@@ -29,14 +18,147 @@ public extension URLSession {
         do {
             let (data, response) = try await self.data(from: url)
             let after = Date()
-
             captureData(request: nil, response: response, before: before, after: after)
-
             return (data, response)
         } catch {
             throw error
         }
     }
+
+    func postHogUpload(for request: URLRequest, fromFile fileURL: URL) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (data, response) = try await upload(for: request, fromFile: fileURL)
+            after = Date()
+            captureData(request: request, response: response, before: before, after: after)
+            return (data, response)
+        } catch {
+            captureData(request: request, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    func postHogUpload(for request: URLRequest, from bodyData: Data) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (data, response) = try await upload(for: request, from: bodyData)
+            after = Date()
+            captureData(request: request, response: response, before: before, after: after)
+            return (data, response)
+        } catch {
+            captureData(request: request, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    func postHogData(for request: URLRequest, delegate: (any URLSessionTaskDelegate)? = nil) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (data, response) = try await self.data(for: request, delegate: delegate)
+            after = Date()
+            captureData(request: request, response: response, before: before, after: after)
+            return (data, response)
+        } catch {
+            captureData(request: request, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    func postHogData(from url: URL, delegate: (any URLSessionTaskDelegate)? = nil) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (data, response) = try await self.data(from: url, delegate: delegate)
+            after = Date()
+            captureData(request: nil, response: response, before: before, after: after)
+            return (data, response)
+        } catch {
+            captureData(request: nil, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    func postHogUpload(for request: URLRequest, fromFile fileURL: URL, delegate: (any URLSessionTaskDelegate)? = nil) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (data, response) = try await upload(for: request, fromFile: fileURL, delegate: delegate)
+            after = Date()
+            captureData(request: request, response: response, before: before, after: after)
+            return (data, response)
+        } catch {
+            captureData(request: request, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    func postHogUpload(for request: URLRequest, from bodyData: Data, delegate: (any URLSessionTaskDelegate)? = nil) async throws -> (Data, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (data, response) = try await upload(for: request, from: bodyData, delegate: delegate)
+            after = Date()
+            captureData(request: request, response: response, before: before, after: after)
+            return (data, response)
+        } catch {
+            captureData(request: request, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    func postHogDownload(for request: URLRequest, delegate: (any URLSessionTaskDelegate)? = nil) async throws -> (URL, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (fileURL, response) = try await download(for: request, delegate: delegate)
+            after = Date()
+            captureData(request: request, response: response, before: before, after: after)
+            return (fileURL, response)
+        } catch {
+            captureData(request: request, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    func postHogDownload(from url: URL, delegate: (any URLSessionTaskDelegate)? = nil) async throws -> (URL, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (fileURL, response) = try await download(from: url, delegate: delegate)
+            after = Date()
+            captureData(request: nil, response: response, before: before, after: after)
+            return (fileURL, response)
+        } catch {
+            captureData(request: nil, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    @available(macOS 12.0, iOS 15.0, watchOS 8.0, tvOS 15.0, *)
+    func postHogDownload(resumeFrom resumeData: Data, delegate: (any URLSessionTaskDelegate)? = nil) async throws -> (URL, URLResponse) {
+        let before = Date()
+        var after: Date?
+        do {
+            let (fileURL, response) = try await download(resumeFrom: resumeData, delegate: delegate)
+            after = Date()
+            captureData(request: nil, response: response, before: before, after: after)
+            return (fileURL, response)
+        } catch {
+            captureData(request: nil, response: nil, before: before, after: after)
+            throw error
+        }
+    }
+
+    // MARK: Private methods
 
     private func captureData(request: URLRequest? = nil, response: URLResponse? = nil, before: Date, after: Date? = nil) {
         // we dont check config.sessionReplayConfig.captureNetworkTelemetry here since this extension

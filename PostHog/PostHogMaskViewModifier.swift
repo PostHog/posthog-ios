098006f1370c86642c204d993239baa2696cb5e0
Manoel Aranda Neto <5731772+marandaneto@users.noreply.github.com>
2024-10-14 15:28:29 +0200

    chore: manually mask swiftui views (#202)
    * chore: manually mask swiftui views
    
    * feat: use custom phIsManuallyMasked property to mark a view for manual masking
    
    * fix: update visibility of phIsManuallyMasked extension
    
    * chore: Update CHANGELOG.md
    
    * fix: remove unused view state property
    
    * chore: remove unused PostHogScreenshotMasker
    
    * chore: remove unused accessibilityIdentifier from SwiftUI sample
    
    * chore: update CHANGELOG.md
    
    * chore: lint
    
    * fix: rollback sample configuration
    
    * chore(iOS): update postHogMask view modifier signature
    
    ---------
    
    Co-authored-by: Ioannis J <yiannis@posthog.com>

---
+++ PostHog/PostHogMaskViewModifier.swift
@@ -0,0 +1,67 @@
+//
+//  PostHogMaskViewModifier.swift
+//  PostHog
+//
+//  Created by Yiannis Josephides on 09/10/2024.
+//
+
+#if os(iOS) && canImport(SwiftUI)
+
+    import SwiftUI
+
+    public extension View {
+        func postHogMask(_ isEnabled: Bool = true) -> some View {
+            modifier(PostHogMaskViewModifier(enabled: isEnabled))
+        }
+    }
+
+    private struct PostHogMaskViewTagger: UIViewRepresentable {
+        func makeUIView(context _: Context) -> PostHogMaskViewTaggerView {
+            PostHogMaskViewTaggerView()
+        }
+
+        func updateUIView(_: PostHogMaskViewTaggerView, context _: Context) {
+            // nothing
+        }
+    }
+
+    private struct PostHogMaskViewModifier: ViewModifier {
+        let enabled: Bool
+
+        func body(content: Content) -> some View {
+            content.background(viewTagger)
+        }
+
+        @ViewBuilder
+        private var viewTagger: some View {
+            if enabled {
+                PostHogMaskViewTagger()
+            }
+        }
+    }
+
+    private class PostHogMaskViewTaggerView: UIView {
+        override func didMoveToSuperview() {
+            super.didMoveToSuperview()
+            superview?.phIsManuallyMasked = true
+        }
+    }
+
+    private var phIsManuallyMaskedKey: UInt8 = 0
+    extension UIView {
+        var phIsManuallyMasked: Bool {
+            get {
+                objc_getAssociatedObject(self, &phIsManuallyMaskedKey) as? Bool ?? false
+            }
+
+            set {
+                objc_setAssociatedObject(
+                    self,
+                    &phIsManuallyMaskedKey,
+                    newValue as Bool?,
+                    .OBJC_ASSOCIATION_RETAIN_NONATOMIC
+                )
+            }
+        }
+    }
+#endif

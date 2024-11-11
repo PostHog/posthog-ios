93c471ac34fbb26da275ddb6ea416a870f9a962d
Ioannis J <yiannis@posthog.com>
2024-11-11 17:40:10 +0200

    fix: wrap in compiler directive

--- PostHog/Utils/UIApplication+.swift
+++ PostHog/Utils/UIApplication+.swift
@@ -5,27 +5,30 @@
 //  Created by Yiannis Josephides on 11/11/2024.
 //
 
-import UIKit
+#if canImport(UIKit)
+    import UIKit
 
-extension UIApplication {
-    static func getCurrentWindow(checkForegrounded: Bool = false) -> UIWindow? {
-        let windowScenes = UIApplication.shared
-            .connectedScenes
-            .compactMap { $0 as? UIWindowScene }
-            .filter {
-                !checkForegrounded || $0.activationState == .foregroundActive
+    extension UIApplication {
+        static func getCurrentWindow(checkForegrounded: Bool = false) -> UIWindow? {
+            let windowScenes = UIApplication.shared
+                .connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .filter {
+                    !checkForegrounded || $0.activationState == .foregroundActive
+                }
+
+            if #available(iOS 15.0, *) {
+                // attempt to retrieve directly from UIWindowScene
+                if let keyWindow = windowScenes.compactMap(\.keyWindow).first {
+                    return keyWindow
+                }
             }
 
-        if #available(iOS 15.0, *) {
-            // attempt to retrieve directly from UIWindowScene
-            if let keyWindow = windowScenes.compactMap(\.keyWindow).first {
-                return keyWindow
-            }
+            // fall back to iterating for `isKeyWindow`
+            return windowScenes
+                .flatMap(\.windows)
+                .first(where: { $0.isKeyWindow })
         }
-
-        // fall back to iterating for `isKeyWindow`
-        return windowScenes
-            .flatMap(\.windows)
-            .first(where: { $0.isKeyWindow })
     }
-}
+
+#endif

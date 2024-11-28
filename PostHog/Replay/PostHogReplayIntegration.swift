59c21458978b3394e62eaceccda8b2defe6ba372
Ioannis J <yiannis@posthog.com>
2024-11-28 08:18:37 +0200

    fix: check for animated transitions only in root vc

--- PostHog/Replay/PostHogReplayIntegration.swift
+++ PostHog/Replay/PostHogReplayIntegration.swift
@@ -363,12 +363,8 @@
         }
 
         private func toScreenshotWireframe(_ window: UIWindow) -> RRWireframe? {
-            if !window.isVisible() {
-                return nil
-            }
-
             // this will bail on interactive animations (e.g edge slide)
-            if isAnimatingTransition(window) {
+            if !window.isVisible() || isAnimatingTransition(window) {
                 return nil
             }
 
@@ -393,18 +389,7 @@
 
         /// Check if window's root view controller is animating a transition
         private func isAnimatingTransition(_ window: UIWindow) -> Bool {
-            guard let viewController = window.rootViewController else { return false }
-
-            return isAnimatingTransition(viewController)
-        }
-
-        // Recursively check if a view controller (or any of its children) is currently animating a transition
-        // During a transition, a UIViewControllerTransitionCoordinator is assigned to a UIViewController which controls the overall transition
-        private func isAnimatingTransition(_ viewController: UIViewController) -> Bool {
-            if let isAnimated = viewController.transitionCoordinator?.isAnimated {
-                return isAnimated
-            }
-            return viewController.children.contains(where: isAnimatingTransition)
+            window.rootViewController?.transitionCoordinator?.isAnimated ?? false
         }
 
         private func isAssetsImage(_ image: UIImage) -> Bool {

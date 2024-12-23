299903b3326e670a983b70e46c6bb3af96bbc487
Ioannis J <yiannis@posthog.com>
2024-12-23 14:23:11 +0200

    fix: add UITextEffectsWindow type in screen ignore list

--- PostHog/Utils/UIWindow+.swift
+++ PostHog/Utils/UIWindow+.swift
@@ -9,9 +9,42 @@
     import Foundation
     import UIKit
 
+    /**
+     Known keyboard window (private) types
+
+     ## UIRemoteKeyboardWindow
+     This is the window that manages the actual keyboard
+     
+     The following system view controllers were observed to be presented in a UIRemoteKeyboardWindow window
+        - UIInputWindowController
+        - UICompatibilityInputViewController
+        - UISystemInputAssistantViewController
+        - UIPredictionViewController
+        - UISystemKeyboardDockController
+        - TUIEmojiSearchInputViewController
+        - STKPrewarmingViewController
+        - STKStickerRemoteSearchViewController
+        - _UIRemoteInputViewController
+        - _UISceneHostingViewController
+        - STKEmojiAndStickerCollectionViewController
+     
+     ## UITextEffectsWindow
+     Hosts system components like the magnifying glass for text selection, predictive text suggestions, copy/paste menus, input accessory views etc.
+     
+     The following system view controllers were observed to be presented in a UITextEffectsWindow window
+        - UIInputWindowController
+        - UICompatibilityInputViewController
+     
+     These view controllers should not appear in a $screen event. If they do, then it means that they are presented in a UIWindow not listed below
+     */
+    private let knownKeyboardWindowTypes: [String] = [
+        "UIRemoteKeyboardWindow",
+        "UITextEffectsWindow",
+    ]
+
     extension UIWindow {
         var isKeyboardWindow: Bool {
-            String(describing: type(of: window)) == "UIRemoteKeyboardWindow"
+            knownKeyboardWindowTypes.contains(String(describing: type(of: self)))
         }
     }
 #endif

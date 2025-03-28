889f89a0da78544df735674d6bd1cb18481817c9
Ioannis J <yiannis@posthog.com>
2025-03-28 10:21:06 +0200

    Update version

--- PostHog/PostHogVersion.swift
+++ PostHog/PostHogVersion.swift
@@ -9,7 +9,7 @@ import Foundation
 
 // if you change this, make sure to also change it in the podspec and check if the script scripts/bump-version.sh still works
 // This property is internal only
-public var postHogVersion = "3.20.1"
+public var postHogVersion = "3.21.0"
 
 public let postHogiOSSdkName = "posthog-ios"
 // This property is internal only

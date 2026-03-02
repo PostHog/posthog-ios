e439eb02fd329431736d69b556b4e1776d2f6309
Ioannis J <yiannis@posthog.com>
2026-03-03 01:59:55 +0200

    chore: refactor exampel

---
+++ PostHogExample/SessionRecordingView.swift
@@ -0,0 +1,85 @@
+//
+//  SessionRecordingView.swift
+//  PostHogExample
+//
+
+import PostHog
+import SwiftUI
+
+struct SessionRecordingView: View {
+    @State private var refreshStatusID = UUID()
+
+    private func refreshStatus() {
+        Task { @MainActor in
+            refreshStatusID = UUID()
+        }
+    }
+
+    private var sessionRecordingStatus: String {
+        if PostHogSDK.shared.isSessionReplayActive() {
+            return "🟢 Recording"
+        } else {
+            return "🔴 Not Recording"
+        }
+    }
+
+    var body: some View {
+        Form {
+            Section("Manual Controls") {
+                Group {
+                    Text("\(sessionRecordingStatus)")
+                    Text("SID: \(PostHogSDK.shared.getSessionId() ?? "NA")")
+                        .lineLimit(1)
+                        .truncationMode(.middle)
+                        .multilineTextAlignment(.leading)
+                }
+                .id(refreshStatusID)
+
+                Button("Stop") {
+                    PostHogSDK.shared.stopSessionRecording()
+                    refreshStatus()
+                }
+                Button("Resume") {
+                    PostHogSDK.shared.startSessionRecording()
+                    refreshStatus()
+                }
+                Button("Start New Session") {
+                    PostHogSDK.shared.startSessionRecording(resumeCurrent: false)
+                    refreshStatus()
+                }
+            }
+
+            Section("Event Trigger") {
+                Button {
+                    PostHogSDK.shared.stopSessionRecording()
+                    PostHogSessionManager.shared.setSessionId(UUID().uuidString)
+                    refreshStatus()
+                } label: {
+                    Text("Stop & Rotate Session Id")
+                }
+
+                Button {
+                    PostHogSDK.shared.capture("start_replay_trigger_1")
+                    refreshStatus()
+                } label: {
+                    Text("Capture 'start_replay_trigger_1'")
+                }
+
+                Button {
+                    PostHogSDK.shared.capture("start_replay_trigger_2")
+                    refreshStatus()
+                } label: {
+                    Text("Capture 'start_replay_trigger_2'")
+                }
+
+                Button {
+                    PostHogSDK.shared.capture("start_replay_trigger_local")
+                    refreshStatus()
+                } label: {
+                    Text("Capture 'start_replay_trigger_local'")
+                }
+            }
+        }
+        .navigationTitle("Session Recording")
+    }
+}

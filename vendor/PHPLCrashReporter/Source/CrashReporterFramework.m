e1f160214e194bbe4516940b7931cde341bdb3b8
James Jackson <james.sorley.jackson@gmail.com>
2026-06-18 09:42:01 -0400

    fix: silence vendored libtool empty-object warnings

--- vendor/PHPLCrashReporter/Source/CrashReporterFramework.m
+++ vendor/PHPLCrashReporter/Source/CrashReporterFramework.m
@@ -2,3 +2,4 @@
  * This empty file is required to correctly resolve
  * all build settings and dependencies in the framework targets.
  */
+__attribute__((used, visibility("hidden"))) const int PHPLCrashReporterFrameworkTranslationUnitAnchor = 0;
